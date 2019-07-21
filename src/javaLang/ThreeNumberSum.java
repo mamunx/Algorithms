d06239dac84313961c8cae6a53cf0af5e51b2147
mamunzx <mamun@field.buzz>
2019-07-21 08:51:20 +0600

    *difficulty and category defined

--- src/javaLang/ThreeNumberSum.java
+++ src/javaLang/ThreeNumberSum.java
@@ -10,6 +10,9 @@ public class ThreeNumberSum {
     /*
      * The Three Number Sum Problem
      *
+     * Category: Array
+     * Difficulty: Medium
+     *
      * Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum.
      * The function should find all triplets in the array that sum up to the target sum and return a two-dimensional
      * array of all these triplets. The numbers in each triplet should be ordered in ascending order, and the triplets
--- src/javaLang/TwoNumberSum.java
+++ src/javaLang/TwoNumberSum.java
@@ -10,6 +10,9 @@ public class TwoNumberSum {
     /*
      * The Two Number Sum Problem
      *
+     * Category: Array
+     * Difficulty: Easy
+     *
      * Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum.
      * If any two numbers in the input array sum up to the target sum, the function should return them in an array,
      * in sorted order. If no two numbers sum up to the target sum, the function should return an empty array.
